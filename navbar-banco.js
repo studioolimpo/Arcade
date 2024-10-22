cf5b552eb3f84935fddda200bdfe2972fcaced0e
studioolimpo <info@studioolimpo.it>
2024-10-22 17:53:24 +0200

    Navbar update

--- navbar-banco.js
+++ navbar-banco.js
@@ -9,13 +9,21 @@ var hamburgerLine = document.querySelectorAll(".nav_hamburger_line");
 var navWrap = document.querySelector(".nav_wrap");
 var logoWrap = document.querySelector(".nav_logo_wrap");
 
+// Acquisisci il valore del tema iniziale di .nav_wrap
+let initialTheme = navWrap.getAttribute("data-theme") || "dark"; // Default to "dark" if not set
+
+// Determina il tema corrente basato sul valore di data-theme
+let currentTheme = initialTheme === "dark" ? "dark-theme" : "brand-theme";
+
+// Imposta le classi iniziali di .nav_wrap in base al tema corrente
+navWrap.classList.add(currentTheme);
+
 // Crea una timeline GSAP per l'animazione del menu
 var tl = gsap.timeline({
   paused: true,
   reversed: true,
   onReverseComplete: function () {
     gsap.set(nav, { display: "none" });
-    // Imposta il mixBlendMode su normal di default quando l'animazione è invertita
     gsap.set(navContain, { css: { mixBlendMode: "normal" } });
     gsap.set(navDivider, { css: { mixBlendMode: "normal" } });
     document.body.style.overflow = ""; // Riattiva lo scroll del body
@@ -26,7 +34,7 @@ var tl = gsap.timeline({
 // Definisci l'animazione iniziale per la timeline
 tl.set(nav, { display: "block" })
   .set(background, {
-    css: { background: "var(--swatch--brand-banco)" },
+    css: { background: "var(--swatch--brand-banco)" }, // Modifica del background
   })
   .from(background, { yPercent: -100, duration: 0.5, ease: "power1.inOut" }, 0)
   .to(hamburgerLine[0], { y: 3.5, rotate: 45, duration: 0.4 }, "<")
@@ -46,7 +54,6 @@ tl.set(nav, { display: "block" })
 
 // Funzione per gestire l'animazione del menu e il mix-blend-mode
 function toggleAnim() {
-  // Toggle class "is-active" su .hamburger e .nav
   hamburger.classList.toggle("is-active");
   nav.classList.toggle("is-active");
 
@@ -62,7 +69,6 @@ function toggleAnim() {
     gsap.set(navDivider, { css: { mixBlendMode: "normal" } });
   }
 
-  // Riproduci o inverti l'animazione
   if (tl.reversed()) {
     tl.play();
     document.body.style.overflow = "hidden"; // Disabilita lo scroll del body
@@ -78,4 +84,87 @@ hamburger.addEventListener("click", toggleAnim);
 // Aggiungi l'evento click su ciascun link del menu mobile
 links.forEach(function (link) {
   link.addEventListener("click", toggleAnim);
+});
+
+// Gestione della visibilità della navbar in base allo scroll
+let bodyScrollDirection;
+let lastScrollTop = 0;
+const scrollThreshold = 20;
+
+// Check start page
+if (window.scrollY > 0) {
+  navWrap.classList.add("is-hidden");
+}
+
+// Hide / show nav on scroll down / up
+ScrollTrigger.create({
+  trigger: "body",
+  start: "top top",
+  end: "bottom bottom",
+  onUpdate: (self) => {
+    let currentScrollTop = self.scroll(); // Ottieni la posizione attuale di scroll
+    if (Math.abs(currentScrollTop - lastScrollTop) > scrollThreshold) {
+      bodyScrollDirection = currentScrollTop > lastScrollTop ? 1 : -1; // 1 per scroll giù, -1 per scroll su
+      lastScrollTop = currentScrollTop; // Aggiorna l'ultima posizione di scroll
+
+      if (bodyScrollDirection === 1) {
+        navWrap.classList.add("is-hidden");
+      } else {
+        navWrap.classList.remove("is-hidden");
+      }
+    }
+  },
+});
+
+// Determina il tema corrente e gestisce le classi
+$("section[data-theme]").each(function () {
+  let theme = 3; // Imposta il tema di default (brand)
+  if ($(this).attr("data-theme") === "dark") theme = 2; // Cambia il tema se è "dark"
+
+  ScrollTrigger.create({
+    trigger: $(this),
+    start: "top top",
+    end: "bottom top",
+    // markers: "true",
+    onToggle: ({ isActive }) => {
+      if (isActive) {
+        // Esegui la transizione dei colori
+        gsap.to(".nav_wrap", { ...colorThemes[theme], duration: 0.3 });
+
+        // Gestisci le classi di tema solo se c'è un cambiamento effettivo
+        if (theme === 1 && currentTheme !== "brand-theme") {
+          $(".nav_wrap").removeClass("dark-theme").addClass("brand-theme");
+          currentTheme = "brand-theme";
+          gsap.set(navContain, { css: { mixBlendMode: "normal" } });
+          gsap.set(navDivider, { css: { mixBlendMode: "normal" } });
+          gsap.set(background, {
+            css: { background: "var(--swatch--brand-banco)" },
+          });
+        } else if (theme === 2 && currentTheme !== "dark-theme") {
+          $(".nav_wrap").removeClass("brand-theme").addClass("dark-theme");
+          currentTheme = "dark-theme";
+          // Imposta mix-blend-mode su "difference" per il tema dark
+          gsap.set(navContain, { css: { mixBlendMode: "difference" } });
+          gsap.set(navDivider, { css: { mixBlendMode: "difference" } });
+          // gsap.set(background, {
+          //  css: { background: "var(--swatch--light)" },
+          // });
+        } else if (theme === 3 && currentTheme !== "brand-theme") {
+          // Se il tema è brand e non è già impostato, mantieni normal
+          $(".nav_wrap").removeClass("dark-theme").addClass("brand-theme");
+          currentTheme = "brand-theme";
+          gsap.set(navContain, { css: { mixBlendMode: "normal" } });
+          gsap.set(navDivider, { css: { mixBlendMode: "normal" } });
+          gsap.set(background, {
+            css: { background: "var(--swatch--brand-banco)" },
+          });
+        }
+      }
+    },
+  });
+});
+
+// Cause a click on menu bg to trigger a click on nav button
+background.addEventListener("click", function () {
+  hamburger.click();
 });
